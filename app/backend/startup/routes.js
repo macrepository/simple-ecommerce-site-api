3cbe67aebcbff7fd3c052ec53eef0a5e77de3efd
Shem <macapobresshem@gmail.com>
2024-02-27 14:41:43 +0800

    Added body parser

--- app/backend/startup/routes.js
+++ app/backend/startup/routes.js
@@ -1,5 +1,8 @@
+const bodyParser = require("koa-bodyparser");
 const customer = require("../resource/customer/routes");
 
 module.exports = function (app) {
+  app.use(bodyParser());
+
   app.use(customer.routes()).use(customer.allowedMethods());
 };
