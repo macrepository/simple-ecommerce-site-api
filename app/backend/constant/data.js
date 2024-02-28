2aa93675f8022c1b9488e8e3a6b82d2d9f584a82
Shem <macapobresshem@gmail.com>
2024-02-29 00:08:53 +0800

    Added specific messages to API response

--- app/backend/constant/data.js
+++ app/backend/constant/data.js
@@ -13,33 +13,49 @@ exports.password = {
 
 exports.httpResponse = {
     success: {
-        code: "success",
-        status: 200,
-        message: "Success."
+      code: "success",
+      status: 200,
+      message: "Request processed successfully."
     },
     badRequest: {
-        code: "bad_request",
-        status: 400,
-        message: "Invalid request."
+      code: "bad_request",
+      status: 400,
+      message: {
+        invalidRequest: "Invalid request.",
+        notSetKey: "Required parameter '%1' is missing.",
+      }
     },
     unauthorized: {
-        code: "unauthorized",
-        status: 401,
-        message: "Unauthorized."
+      code: "unauthorized",
+      status: 401,
+      message: {
+        invalidLogin: "Invalid login credentials",
+        noToken: "Access denied. No token provided."
+      }
     },
     forbidden: {
-        code: "forbidden",
-        status: 403,
-        message: "Forbidden."
+      code: "forbidden",
+      status: 403,
+      message: "Forbidden."
     },
     notFound: {
-        code: "not_found",
-        status: 404,
-        message: "Not found."
+      code: "not_found",
+      status: 404,
+      message: "Resource not found."
+    },
+    conflict: {
+      code: "conflict",
+      status: 409,
+      message: {
+        saveFailed: "Failed to create a new %1 record.",
+        updateFailed: "Failed to update %1 record.",
+        deleteFailed: "Failed to delete %1 record."
+      }
     },
     internalServerError: {
-        code: "internal_server_error",
-        status: 500,
-        message: "Server error."
+      code: "internal_server_error",
+      status: 500,
+      message: "Server error. Please try again later."
     }
-}
+  };
+  
