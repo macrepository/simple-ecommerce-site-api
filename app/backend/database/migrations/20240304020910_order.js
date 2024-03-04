1bc7e566d2495fd18329f76267f768dafe1a1d87
Shem <macapobresshem@gmail.com>
2024-03-04 11:55:35 +0800

    Added Order schema

---
+++ app/backend/database/migrations/20240304020910_order.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("order", (table) => {
+      table.increments("id").primary().comment("Order ID");
+      table.integer("customer_id").comment("Customer ID");
+      table.integer("quote_id").comment("Quote ID");
+      table.enu('status', ["pending", "processing", "complete", "cancelled"]).comment("Order Status");
+      table.string("first_name", 50).notNullable().comment("Customer First Name");
+      table.string("last_name", 50).notNullable().comment("Customer Last Name");
+      table.date("date_of_birth").comment("Customer Date of Birth");
+      table
+        .enu("gender", ["male", "female", "others"])
+        .comment("Customer Gender");
+      table.string("address", 255).comment("Customer Address");
+      table.string("zip_code", 20).comment("Customer Zip Code");
+      table
+        .string("email", 50)
+        .notNullable()
+        .unique()
+        .comment("Customer Email Address");
+      table.float("subtotal", 9, 2).notNullable().comment("Order Subtotal");
+      table.float("grandtotal", 9, 2).notNullable().comment("Order Grandtotal");
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("order");
+  };
+  
