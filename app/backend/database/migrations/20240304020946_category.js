3476bbb0aac2eda445a80d699101203c0021c8a2
Shem <macapobresshem@gmail.com>
2024-03-04 11:55:48 +0800

    added product and category schema

---
+++ app/backend/database/migrations/20240304020946_category.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("category", (table) => {
+    table.increments("id").primary().comment("Category ID");
+    table.string("name", 50).notNullable().comment("Category Name");
+    table.string("thumbnail", 50).notNullable().comment("Category Thumbnail");
+    table.boolean("is_active").notNullable().comment("Category Status");
+    table.timestamp(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("category");
+};
---
+++ app/backend/database/migrations/20240304020952_product.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("product", (table) => {
+      table.increments("id").primary().comment("Product ID");
+      table.integer("category_id").unsigned().comment("Category Reference");
+      table.foreign("category_id").references("category.id");
+      table.string("name", 50).notNullable().comment("Product Name");
+      table.float("price", 9, 2).notNullable().comment("Product Price");
+      table.integer("quantity", 6).notNullable().comment("Product Quantity");
+      table.text("description").comment("Product Description");
+      table.boolean("is_active").notNullable().comment("Product Active Status");
+      table.string("thumbnail", 50).notNullable().comment("Product Thumbnail");
+      table.text("gallery").comment("Product Gallery Path in JSON Format");
+      table.timestamp(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("product");
+  };
+  
