4c8610700da703bd5e7ee8495756e4cf611c4bb4
Shem <macapobresshem@gmail.com>
2024-03-04 11:56:01 +0800

    added user admin schema

---
+++ app/backend/database/migrations/20240304020957_user.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id").primary().comment("User ID");
+    table.string("first_name", 50).notNullable().comment("User First Name");
+    table.string("last_name", 50).notNullable().comment("User Last Name");
+    table
+      .string("email", 50)
+      .notNullable()
+      .unique()
+      .comment("User Email Address");
+    table.string("username", 20).notNullable().comment("User Username");
+    table.string("password", 255).notNullable().comment("User Login Password");
+    table.enu("role", ["admin", "order_management", "product_management"]);
+    table.timestamp(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("user");
+};
