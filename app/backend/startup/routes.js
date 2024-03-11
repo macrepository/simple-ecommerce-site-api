9f74061071f36c108b7f5b550b57f51df1437559
Shem <macapobresshem@gmail.com>
2024-03-11 17:54:08 +0800

    Register order routes to startup

--- app/backend/startup/routes.js
+++ app/backend/startup/routes.js
@@ -1,5 +1,6 @@
 const bodyParser = require("koa-bodyparser");
 const customer = require("../resource/customer/routes");
+const order = require("../resource/order/routes");
 const quote = require("../resource/quote/routes");
 const { catchErrors } = require("../middleware/async-exception-handler");
 
@@ -10,4 +11,5 @@ module.exports = function (app) {
 
   app.use(customer.routes()).use(customer.allowedMethods());
   app.use(quote.routes()).use(quote.allowedMethods());
+  app.use(order.routes()).use(order.allowedMethods());
 };
