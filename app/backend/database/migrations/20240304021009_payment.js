7fb9e381eba6022c77e3f91fb63f33cbfc6f5bc4
Shem <macapobresshem@gmail.com>
2024-03-12 12:06:02 +0800

    Added additional payment table column

--- app/backend/database/migrations/20240304021009_payment.js
+++ app/backend/database/migrations/20240304021009_payment.js
@@ -5,6 +5,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable("payment", (table) => {
     table.increments("id").primary().comment("Payment ID");
+    table
+      .boolean("is_active")
+      .notNullable()
+      .comment("Payment Method Active Status");
     table.string("method", 10).notNullable().comment("Payment Method Code");
     table.string("name", 50).notNullable().comment("Payment Method Name");
     table.timestamps(true, true);
