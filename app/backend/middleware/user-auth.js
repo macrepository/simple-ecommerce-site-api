427cda36a8f551df5f4694f2c272628f06cebc09
Shem <macapobresshem@gmail.com>
2024-03-12 11:40:52 +0800

    Added user middleware

---
+++ app/backend/middleware/user-auth.js
@@ -0,0 +1,27 @@
+const { response } = require("../utilities/http-response");
+const { verifyToken } = require("../utilities/json-web-token");
+const { httpResponse } = require("../constant/data");
+
+module.exports.userAuth = function (ctx, next) {
+  const token = ctx.get("x-auth-token");
+
+  if (!token) {
+    return response(
+      ctx,
+      httpResponse.unauthorized,
+      httpResponse.unauthorized.message.noToken
+    );
+  }
+
+  const decoded = verifyToken(token);
+  if (!decoded) {
+    return response(
+      ctx,
+      httpResponse.unauthorized,
+      httpResponse.unauthorized.message.noToken
+    );
+  }
+
+  ctx.request.body = { user: decoded };
+  next();
+};
